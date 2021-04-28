f26d7deea68995d036def6f7dbe1964892c8473d
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-04-28 18:44:17 +0430

    coloring unsupported in windows
    in windows cmd you don't can color the texts print real thing that you wrote here

--- src/error.cpp
+++ src/error.cpp
@@ -1,8 +1,16 @@
 #include "error.h"
+#ifdef _WIN32
+#define color 0
+#else
+#define color 1
+#endif
 
 void Error::compiler(ERRORS code, const char *message)
 {
-    std::cerr << "\x1b[0;91;1mERROR[E" << std::setfill('0') << std::setw(3) << code << "]\x1b[0;1m: " << message << std::endl;
+    if(color){
+        std::cerr << "\x1b[0;91;1mERROR[E" << std::setfill('0') << std::setw(3) << code << "]\x1b[0;1m: " << message << std::endl;
+    }else {
+        std::cerr "E" << std::setfill('0') << std::setw(3) << code << "]:" << message << std::endl;
     exit(1);
 }
 
